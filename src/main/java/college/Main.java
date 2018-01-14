8eada3a089d544964019d3e9a4dad734eeda93e8
FromSi <fromsi665@gmail.com>
2018-01-14 18:37:58 +0600

    Add notification

--- src/main/java/college/Main.java
+++ src/main/java/college/Main.java
@@ -3,6 +3,7 @@ package college;
 import college.jdbc.*;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static spark.Spark.*;
@@ -10,10 +11,44 @@ import static spark.Spark.*;
 public class Main {
     private static List<UsersCBK> usersCBK;
     public static void main(String[] args) throws URISyntaxException {
-
-
+        port(getHerokuAssignedPort());
+        usersCBK = new ArrayList<>();
         usersCBK = new JDBCBEGIN().addNotifAll();
-        new REST().start(usersCBK);
+        get("/", (request, response) -> usersCBK.get(0).getIdUser() + " --- "+usersCBK.get(1).getIdUser());
+
+        post("/user", (request, response) -> new JDBCPOST().createUser(request));
+        post("/dialog", (request, response) -> {
+            for (int i = 0; i < usersCBK.size(); i++) {
+                if (usersCBK.get(i).getIdUser().equals(request.queryParams("idoutgoing"))) {
+                    usersCBK.get(i).setNotification(true);
+                    return new JDBCPOST().createMS(request);
+                }
+            }
+            return "null";
+        });
+        post("/friend", (request, response) -> new JDBCPOST().createFriends(request));
+        get("/dialog", (request, response) -> new JDBCGET().printMessage(request));
+        get("/notification", (request, response) -> {
+            for (int i = 0; i < usersCBK.size(); i++) {
+                if (usersCBK.get(i).getIdUser().equals(request.queryParams("idincoming")))
+                    if (usersCBK.get(i).isNotification()) {
+                        usersCBK.get(i).setNotification(false);
+                        return new JDBCGET().printMessage(request);
+                    }
+            }
+            return "null";
+        });
+        get("/friend", (request, response) -> new JDBCGET().printFriends(request));
+        get("/profile", (request, response) -> new JDBCGET().printUser(request));
+        get("/login", (request, response) -> new JDBCGET().printLogin(request));
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
 
 }
